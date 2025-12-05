8f45cfd99141dd905bd05334a9705ca13fbe44d3
johnsydev <johnsylopez02@gmail.com>
2025-12-05 12:56:40 -0600

    Inicio de DimDate, inclusión de feriados de CR, FactOrders en DW
    Co-authored-by: Sergio Montoya <sermontoya@estudiantec.cr>

--- Script sql/Create_BikeStoresDW.sql
+++ Script sql/Create_BikeStoresDW.sql
@@ -57,13 +57,14 @@ CREATE TABLE DimCustomers (
 go
 
 
+-- revisar
 CREATE TABLE DimOrders (
 	order_key INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
     order_id INT NOT NULL,
-    customer_id INT NOT NULL,
-    order_status INT NOT NULL,
-    store_id INT NOT NULL,
-    staff_id INT NOT NULL
+    --customer_id INT NOT NULL,
+    --order_status INT NOT NULL,
+    --store_id INT NOT NULL,
+    --staff_id INT NOT NULL
 )
 go
 
@@ -107,8 +108,61 @@ go
 
 
 
+-- SALES
+CREATE TABLE dbo.FactOrders (
+	sales_key INT IDENTITY(1,1) NOT NULL,
+	product_key INT NOT NULL,
+	--SupplierKey INT NOT NULL,
+	customer_key INT NOT NULL,
+	staff_key INT NOT NULL,
+	--ShipperKey INT NOT NULL,
+	order_key INT NOT NULL,
+	order_date_key INT NOT NULL, -- ESTO VA CON DimDate
+	RequiredDateKey INT NOT NULL, -- ESTO VA CON DimDate
+	ShippedDateKey INT NOT NULL, -- ESTO VA CON DimDate
+	--QuantityPerUnit nvarchar(40) not null, 
+	list_price DECIMAL(10,2) not null,
+	--UnitsInStock smallint not null,
+	quantity_stock INT not null, -- esto se ocupa?
+	quantity_order INT not null,
+	--Freight money not null, 
+	discount DECIMAL(5,2) NOT NULL,
+	--Quantity smallint not null, -- TRES QUANTITY?
+	--UnitPriceOrder money not null --UnitPrice?
 
-
+PRIMARY KEY CLUSTERED 
+(
+	sales_key ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE dbo.FactOrders  WITH CHECK ADD FOREIGN KEY(customer_key)
+REFERENCES dbo.DimCustomers (customer_key)
+GO
+ALTER TABLE dbo.FactOrders  WITH CHECK ADD FOREIGN KEY(staff_key)
+REFERENCES dbo.DimEmployees (staff_key)
+GO
+ALTER TABLE dbo.FactOrders  WITH CHECK ADD FOREIGN KEY(order_key)
+REFERENCES dbo.DimOrders (order_key)
+GO
+ALTER TABLE dbo.FactOrders  WITH CHECK ADD FOREIGN KEY(OrderDateKey) --DimDate
+REFERENCES dbo.DimDate (DateKey)
+GO
+ALTER TABLE dbo.FactOrders  WITH CHECK ADD FOREIGN KEY(product_key)
+REFERENCES dbo.DimProducts (product_key)
+GO
+ALTER TABLE dbo.FactOrders  WITH CHECK ADD FOREIGN KEY(RequiredDateKey) --DimDate
+REFERENCES dbo.DimDate (DateKey)
+GO
+ALTER TABLE dbo.FactOrders  WITH CHECK ADD FOREIGN KEY(ShippedDateKey) --DimDate
+REFERENCES dbo.DimDate (DateKey)
+GO
+--ALTER TABLE dbo.FactOrders  WITH CHECK ADD FOREIGN KEY(SupplierKey)
+--REFERENCES dbo.DimSuppliers (SuppliersKey)
+--GO
+--ALTER TABLE dbo.FactOrders  WITH CHECK ADD FOREIGN KEY(ShipperKey)
+--REFERENCES dbo.DimShippers (ShippersKey)
+--GO
 
 
 
---
+++ Script sql/Populate_DimDate.sql
@@ -0,0 +1,258 @@
+/********************************************************************************************/
+--Specify Start Date and End date here
+--Value of Start Date Must be Less than Your End Date 
+
+DECLARE @StartDate DATETIME = '01/01/1900' --Starting value of Date Range
+DECLARE @EndDate DATETIME = '01/02/1900' --End Value of Date Range
+
+--Temporary Variables To Hold the Values During Processing of Each Date of Year
+DECLARE
+	@DayOfWeekInMonth INT,
+	@DayOfWeekInYear INT,
+	@DayOfQuarter INT,
+	@WeekOfMonth INT,
+	@CurrentYear INT,
+	@CurrentMonth INT,
+	@CurrentQuarter INT
+
+/*Table Data type to store the day of week count for the month and year*/
+DECLARE @DayOfWeek TABLE (DOW INT, MonthCount INT, QuarterCount INT, YearCount INT)
+
+INSERT INTO @DayOfWeek VALUES (1, 0, 0, 0)
+INSERT INTO @DayOfWeek VALUES (2, 0, 0, 0)
+INSERT INTO @DayOfWeek VALUES (3, 0, 0, 0)
+INSERT INTO @DayOfWeek VALUES (4, 0, 0, 0)
+INSERT INTO @DayOfWeek VALUES (5, 0, 0, 0)
+INSERT INTO @DayOfWeek VALUES (6, 0, 0, 0)
+INSERT INTO @DayOfWeek VALUES (7, 0, 0, 0)
+
+--Extract and assign various parts of Values from Current Date to Variable
+
+DECLARE @CurrentDate AS DATETIME = @StartDate
+SET @CurrentMonth = DATEPART(MM, @CurrentDate)
+SET @CurrentYear = DATEPART(YY, @CurrentDate)
+SET @CurrentQuarter = DATEPART(QQ, @CurrentDate)
+
+/********************************************************************************************/
+--Proceed only if Start Date(Current date ) is less than End date you specified above
+
+WHILE @CurrentDate < @EndDate
+BEGIN
+ 
+/*Begin day of week logic*/
+
+         /*Check for Change in Month of the Current date if Month changed then 
+          Change variable value*/
+	IF @CurrentMonth != DATEPART(MM, @CurrentDate) 
+	BEGIN
+		UPDATE @DayOfWeek
+		SET MonthCount = 0
+		SET @CurrentMonth = DATEPART(MM, @CurrentDate)
+	END
+
+        /* Check for Change in Quarter of the Current date if Quarter changed then change 
+         Variable value*/
+
+	IF @CurrentQuarter != DATEPART(QQ, @CurrentDate)
+	BEGIN
+		UPDATE @DayOfWeek
+		SET QuarterCount = 0
+		SET @CurrentQuarter = DATEPART(QQ, @CurrentDate)
+	END
+       
+        /* Check for Change in Year of the Current date if Year changed then change 
+         Variable value*/
+	
+
+	IF @CurrentYear != DATEPART(YY, @CurrentDate)
+	BEGIN
+		UPDATE @DayOfWeek
+		SET YearCount = 0
+		SET @CurrentYear = DATEPART(YY, @CurrentDate)
+	END
+	
+        -- Set values in table data type created above from variables 
+
+	UPDATE @DayOfWeek
+	SET 
+		MonthCount = MonthCount + 1,
+		QuarterCount = QuarterCount + 1,
+		YearCount = YearCount + 1
+	WHERE DOW = DATEPART(DW, @CurrentDate)
+
+	SELECT
+		@DayOfWeekInMonth = MonthCount,
+		@DayOfQuarter = QuarterCount,
+		@DayOfWeekInYear = YearCount
+	FROM @DayOfWeek
+	WHERE DOW = DATEPART(DW, @CurrentDate)
+	
+/*End day of week logic*/
+
+
+/* Populate Your Dimension Table with values*/
+	
+	INSERT INTO [dbo].[DimDate]
+	SELECT
+		
+		CONVERT (char(8),@CurrentDate,112) as DateKey,
+		@CurrentDate AS Date,
+		CONVERT (char(10),@CurrentDate,103) as FullDateUK,
+		CONVERT (char(10),@CurrentDate,101) as FullDateUSA,
+		DATEPART(DD, @CurrentDate) AS DayOfMonth,
+		--Apply Suffix values like 1st, 2nd 3rd etc..
+		CASE 
+			WHEN DATEPART(DD,@CurrentDate) IN (11,12,13) 
+			THEN CAST(DATEPART(DD,@CurrentDate) AS VARCHAR) + 'th'
+			WHEN RIGHT(DATEPART(DD,@CurrentDate),1) = 1 
+			THEN CAST(DATEPART(DD,@CurrentDate) AS VARCHAR) + 'st'
+			WHEN RIGHT(DATEPART(DD,@CurrentDate),1) = 2 
+			THEN CAST(DATEPART(DD,@CurrentDate) AS VARCHAR) + 'nd'
+			WHEN RIGHT(DATEPART(DD,@CurrentDate),1) = 3 
+			THEN CAST(DATEPART(DD,@CurrentDate) AS VARCHAR) + 'rd'
+			ELSE CAST(DATEPART(DD,@CurrentDate) AS VARCHAR) + 'th' 
+			END AS DaySuffix,
+		
+		DATENAME(DW, @CurrentDate) AS DayName,
+		DATEPART(DW, @CurrentDate) AS DayOfWeekUSA,
+
+		-- check for day of week as Per US and change it as per UK format 
+		CASE DATEPART(DW, @CurrentDate)
+			WHEN 1 THEN 7
+			WHEN 2 THEN 1
+			WHEN 3 THEN 2
+			WHEN 4 THEN 3
+			WHEN 5 THEN 4
+			WHEN 6 THEN 5
+			WHEN 7 THEN 6
+			END 
+			AS DayOfWeekUK,
+		
+		@DayOfWeekInMonth AS DayOfWeekInMonth,
+		@DayOfWeekInYear AS DayOfWeekInYear,
+		@DayOfQuarter AS DayOfQuarter,
+		DATEPART(DY, @CurrentDate) AS DayOfYear,
+		DATEPART(WW, @CurrentDate) + 1 - DATEPART(WW, CONVERT(VARCHAR, 
+		DATEPART(MM, @CurrentDate)) + '/1/' + CONVERT(VARCHAR, 
+		DATEPART(YY, @CurrentDate))) AS WeekOfMonth,
+		(DATEDIFF(DD, DATEADD(QQ, DATEDIFF(QQ, 0, @CurrentDate), 0), 
+		@CurrentDate) / 7) + 1 AS WeekOfQuarter,
+		DATEPART(WW, @CurrentDate) AS WeekOfYear,
+		DATEPART(MM, @CurrentDate) AS Month,
+		DATENAME(MM, @CurrentDate) AS MonthName,
+		CASE
+			WHEN DATEPART(MM, @CurrentDate) IN (1, 4, 7, 10) THEN 1
+			WHEN DATEPART(MM, @CurrentDate) IN (2, 5, 8, 11) THEN 2
+			WHEN DATEPART(MM, @CurrentDate) IN (3, 6, 9, 12) THEN 3
+			END AS MonthOfQuarter,
+		DATEPART(QQ, @CurrentDate) AS Quarter,
+		CASE DATEPART(QQ, @CurrentDate)
+			WHEN 1 THEN 'First'
+			WHEN 2 THEN 'Second'
+			WHEN 3 THEN 'Third'
+			WHEN 4 THEN 'Fourth'
+			END AS QuarterName,
+		DATEPART(YEAR, @CurrentDate) AS Year,
+		'CY ' + CONVERT(VARCHAR, DATEPART(YEAR, @CurrentDate)) AS YearName,
+		LEFT(DATENAME(MM, @CurrentDate), 3) + '-' + CONVERT(VARCHAR, 
+		DATEPART(YY, @CurrentDate)) AS MonthYear,
+		RIGHT('0' + CONVERT(VARCHAR, DATEPART(MM, @CurrentDate)),2) + 
+		CONVERT(VARCHAR, DATEPART(YY, @CurrentDate)) AS MMYYYY,
+		CONVERT(DATETIME, CONVERT(DATE, DATEADD(DD, - (DATEPART(DD, 
+		@CurrentDate) - 1), @CurrentDate))) AS FirstDayOfMonth,
+		CONVERT(DATETIME, CONVERT(DATE, DATEADD(DD, - (DATEPART(DD, 
+		(DATEADD(MM, 1, @CurrentDate)))), DATEADD(MM, 1, 
+		@CurrentDate)))) AS LastDayOfMonth,
+		DATEADD(QQ, DATEDIFF(QQ, 0, @CurrentDate), 0) AS FirstDayOfQuarter,
+		DATEADD(QQ, DATEDIFF(QQ, -1, @CurrentDate), -1) AS LastDayOfQuarter,
+		CONVERT(DATETIME, '01/01/' + CONVERT(VARCHAR, DATEPART(YY, 
+		@CurrentDate))) AS FirstDayOfYear,
+		CONVERT(DATETIME, '12/31/' + CONVERT(VARCHAR, DATEPART(YY, 
+		@CurrentDate))) AS LastDayOfYear,
+		NULL AS IsHolidayUSA,
+		CASE DATEPART(DW, @CurrentDate)
+			WHEN 1 THEN 0
+			WHEN 2 THEN 1
+			WHEN 3 THEN 1
+			WHEN 4 THEN 1
+			WHEN 5 THEN 1
+			WHEN 6 THEN 1
+			WHEN 7 THEN 0
+			END AS IsWeekday,
+		NULL AS HolidayUSA, Null, Null
+
+	SET @CurrentDate = DATEADD(DD, 1, @CurrentDate)
+END
+
+/********************************************************************************************/
+ 
+--Step 3.
+-- FERIADOS OFICIALES DE COSTA RICA
+	
+-- Año nuevo, 01 de enero
+	UPDATE [dbo].[DimDate]
+		SET Holiday = 'Año nuevo'
+	WHERE [Month] = 1 AND [DayOfMonth]  = 1
+
+-- Batalla de Rivas, 11 de Abril
+	UPDATE [dbo].[DimDate]
+		SET Holiday = 'Batalla de Rivas'
+	WHERE [Month] = 4 AND [DayOfMonth]  = 11
+
+-- Jueves Santo de la Semana Santa, 17 de Abril
+   UPDATE [dbo].[DimDate]
+		SET Holiday = 'Jueves Santo - Semana Santa'
+	WHERE [Month] = 4 AND [DayOfMonth]  = 17
+
+-- Viernes Santo de la Semana Santa, 18 de Abril
+   UPDATE [dbo].[DimDate]
+		SET Holiday = 'Viernes Santo - Semana Santa'
+	WHERE [Month] = 4 AND [DayOfMonth]  = 18
+
+-- Día del Trabajador, 1 de mayo 
+    UPDATE [dbo].[DimDate]
+		SET Holiday = 'Día del Trabajador'
+	WHERE [Month] = 5 AND [DayOfMonth]  = 1
+
+-- Anexión de Nicoya, 25 de julio 
+    UPDATE [dbo].[DimDate]
+		SET Holiday = 'Anexión de Nicoya'
+	WHERE [Month] = 7 AND [DayOfMonth]  = 25
+
+-- Día de la Virgen de los Ángeles, 02 de agosto 
+    UPDATE [dbo].[DimDate]
+		SET Holiday = 'Día de la Virgen de los Ángeles'
+	WHERE [Month] = 8 AND [DayOfMonth]  = 2
+
+-- Día de la madre, 15 de agosto 
+    UPDATE [dbo].[DimDate]
+		SET Holiday = 'Día de la madre'
+	WHERE [Month] = 8 AND [DayOfMonth]  = 15
+
+-- Día de la persona negra y la cultura afrodescendiente, 31 de agosto 
+    UPDATE [dbo].[DimDate]
+		SET Holiday = 'Día de la persona negra y la cultura afrodescendiente'
+	WHERE [Month] = 8 AND [DayOfMonth]  = 31
+
+-- Día de la Independencia, 15 de setiembre 
+    UPDATE [dbo].[DimDate]
+		SET Holiday = 'Día de la Independencia'
+	WHERE [Month] = 9 AND [DayOfMonth]  = 15
+
+-- Día de la abolición del ejercito
+	UPDATE [dbo].[DimDate]
+		SET HolidayUK = 'Día de la abolición del ejercito'
+	WHERE [Month] = 12 AND [DayOfMonth]  = 1
+
+-- Navidad
+	UPDATE [dbo].[DimDate]
+		SET HolidayUK = 'Navidad'
+	WHERE [Month] = 12 AND [DayOfMonth]  = 25
+
+--Update flag for Holidays 1= Holiday, 0=No Holiday
+	UPDATE [dbo].[DimDate]
+		SET IsHoliday  = CASE WHEN Holiday   IS NULL 
+		THEN 0 WHEN Holiday   IS NOT NULL THEN 1 END
+		
+ 
+SELECT * FROM [dbo].[DimDate]
